e8cd8fa1d296f329b9c380af5a5e2a0fdef3d5dc
DanShaul <dansh7836@gmail.com>
2023-10-11 15:13:48 +0300

    feat(FeedItem.cs): feed item model for Feed service

---
+++ src/Danstagram.Interactions.Service/Entities/FeedItem.cs
@@ -0,0 +1,13 @@
+using System;
+using Danstagram.Common;
+
+namespace Danstagram.Identity.Service.Entities{
+    public class FeedItem:IEntity{
+        
+        #region Properties
+        
+        public Guid Id{get;set;}
+        
+        #endregion
+    }
+}
