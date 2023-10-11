95265e581668acd1f8c127d0378971f3a5de85de
DanShaul <dansh7836@gmail.com>
2023-10-11 16:28:29 +0300

    feat(controller.cs): implemented all the http methods

--- src/Danstagram.Interactions.Service/Controllers/InteractionsController.cs
+++ src/Danstagram.Interactions.Service/Controllers/InteractionsController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Danstagram.Common;
 using Danstagram.Interactions.Service.Entities;
@@ -14,7 +15,7 @@ namespace Danstagram.Interactions.Service.Controllers{
 
             this.feedItemsRepository = feedItemsRepository;
             this.commentsRepository = commentsRepository;
-            this.likesRespository = likesRepository;
+            this.likesRepository = likesRepository;
             this.identitiesRepository = identitiesRepository;
 
         }
@@ -22,7 +23,7 @@ namespace Danstagram.Interactions.Service.Controllers{
         #region Properties
         private readonly IRepository<FeedItem> feedItemsRepository;
         private readonly IRepository<Comment> commentsRepository;
-        private readonly IRepository<Like> likesRespository;
+        private readonly IRepository<Like> likesRepository;
         private readonly IRepository<Identity> identitiesRepository;
 
         #endregion
@@ -30,21 +31,122 @@ namespace Danstagram.Interactions.Service.Controllers{
 
         [Route("likes/feeditems/{id}")]
         [HttpGet]
-        public async Task<IEnumerable<LikeDto>> GetLikesByFeedItemsIdAsyc(Guid feedItemId){
+        public async Task<IEnumerable<LikeDto>> GetLikesByFeedItemsIdAsync(Guid feedItemId){
             
-            return await likesRespository.GetAllAsync(like => like.FeedItemId == feedItemId);
+            return (await likesRepository.GetAllAsync(like => like.FeedItemId == feedItemId)).Select(like => like.AsDto());
+        }
+
+        [Route("likes/{id}")]
+        [HttpGet]
+        public async Task<ActionResult<LikeDto>> GetLikeByIdAsync(Guid likeId){
+            var like = await likesRepository.GetAsync(likeId);
+
+            if (like == null) return NotFound();
+
+            return like.AsDto();
+
+        }
+
+
+        [Route("comments/{id}")]
+        [HttpGet]
+        public async Task<ActionResult<CommentDto>> GetCommentByIdAsync(Guid commentId){
+            var comment = await commentsRepository.GetAsync(commentId);
+
+            if (comment == null) return NotFound();
+
+            var userName = (await identitiesRepository.GetAsync(comment.UserId)).UserName;
+
+            var commentDto = new CommentDto(
+                comment.Id,
+                userName,
+                comment.FeedItemId,
+                comment.Message,
+                comment.CreatedDate);
+
+            return commentDto;
+
         }
 
         [Route("comments/feeditems/{id}")]
         [HttpGet]
-        public async Task<IEnumerable<CommentDto>> GetCommentsByFeedItemsIdAsyc(Guid feedItemId){
-            return (await commentsRepository.GetAllAsync(comment => comment.FeedItemId == feedItemId)).AsDto();
+        public async Task<IEnumerable<CommentDto>> GetCommentsByFeedItemsIdAsync(Guid feedItemId){
+            var comments = await commentsRepository.GetAllAsync(comment => comment.FeedItemId == feedItemId);
+
+            var commentUserIds = comments.Select(x => x.UserId);
+            var identities = await identitiesRepository.GetAllAsync(identity => commentUserIds.Contains(identity.Id));
+
+
+            var feedComments = comments.Select(comment => new CommentDto(
+                comment.Id,
+                identities.SingleOrDefault(Identity => Identity.Id == comment.UserId).UserName,
+                comment.FeedItemId,
+                comment.Message,
+                comment.CreatedDate));
+
+            return feedComments;
+
         }
 
         [Route("comments")]
         [HttpPost]
-        public 
+        public async Task<ActionResult<CommentDto>> PostCommentAsync(CreateCommentDto createComment){
+            Comment comment = new(){
+                Id = Guid.NewGuid(),
+                CreatedDate = DateTimeOffset.UtcNow,
+                FeedItemId = createComment.FeedItemId,
+                Message = createComment.Message,
+                UserId = createComment.UserId
+            };
+            var validationResponse = await ValidateInteractionAsync<Comment>(comment);
+            if (validationResponse != null) return validationResponse;
 
+            await commentsRepository.CreateAsync(comment);
+
+            return CreatedAtAction(nameof(GetCommentByIdAsync),new{id = comment.Id},comment);
+        }
+
+        [Route("likes")]
+        [HttpPost]
+        public async Task<ActionResult<LikeDto>> PostLikeAsync(CreateLikeDto createLike){
+            Like like = new(){
+                Id = Guid.NewGuid(),
+                FeedItemId = createLike.FeedItemId,
+                UserId = createLike.UserId
+            };
+
+            var validationResponse = await ValidateInteractionAsync<Like>(like);
+            if (validationResponse != null) return validationResponse;
+
+            await likesRepository.CreateAsync(like);
+
+            return CreatedAtAction(nameof(GetLikeByIdAsync),new{Id = like.Id},like);
+        }
+
+        [Route("likes/{id}")]
+        [HttpDelete]
+        public async Task<ActionResult> DeleteLikeAsync(Guid id) {
+            if(likesRepository.GetAsync(id) == null) return NoContent();
+            await likesRepository.RemoveAsync(id);
+
+            return Ok();
+        }
+
+        [Route("comments/{id}")]
+        [HttpDelete]
+        public async Task<ActionResult> DeleteCommentsAsync(Guid id) {
+            if(commentsRepository.GetAsync(id) == null) return NoContent();
+            await commentsRepository.RemoveAsync(id);
+
+            return Ok();
+        }
+        private async Task<ActionResult> ValidateInteractionAsync<T>(T t) where T : Interaction{
+            if((await feedItemsRepository.GetAsync(t.FeedItemId)) == null) return NotFound("Feed item not found");
+
+            if((await identitiesRepository.GetAsync(t.UserId)) == null) return NotFound("User not found");
+
+            return null;
+        }
         #endregion
     }
 }
