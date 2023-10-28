64850241d35b2a8a0b19e7b55a0847f83635dfee
DanShaul <dansh7836@gmail.com>
2023-10-28 21:26:24 +0300

    saving state

--- src/Danstagram.Interactions.Service/Consumers/FeedItemDeletedConsumer.cs
+++ src/Danstagram.Interactions.Service/Consumers/FeedItemDeletedConsumer.cs
@@ -1,53 +1,59 @@
 using System.Threading.Tasks;
 using Danstagram.Common;
 using Danstagram.Feed.Contracts;
+using Danstagram.Interactions.Contracts;
 using Danstagram.Interactions.Service.Entities;
 using MassTransit;
-using MassTransit.Transports.Fabric;
 
-namespace Danstagram.Interactions.Service.Consumers{
-    public class FeedItemDeletedConsumer : RepositoryConsumer<FeedItem>, IConsumer<FeedItemDeleted>
+namespace Danstagram.Interactions.Service.Consumers
+{
+  public class FeedItemDeletedConsumer : RepositoryConsumer<FeedItem>, IConsumer<FeedItemDeleted>
+  {
+    #region Constructors
+    public FeedItemDeletedConsumer(IRepository<FeedItem> repository, IRepository<Like> likesRepository, IRepository<Comment> commentsRepository, IPublishEndpoint publishEndpoint) : base(repository)
     {
-        #region Constructors
-        public FeedItemDeletedConsumer(IRepository<FeedItem> repository,IRepository<Like> likesRepository,IRepository<Comment> commentsRepository, IPublishEndpoint publishEndpoint) : base(repository)
-        {
-            this.likesRepository = likesRepository;
-            this.commentsRepository = commentsRepository;
-            this.publishEndpoint = publishEndpoint;
-        }
-        #endregion
-
-        #region Properties
-        private readonly IRepository<Like> likesRepository;
-        private readonly IRepository<Comment> commentsRepository;
-        private readonly IPublishEndpoint publishEndpoint;
-        #endregion
-
-        #region Methods
-
-        public async Task Consume(ConsumeContext<FeedItemDeleted> context)
-        {
-            var message = context.Message;
-            if((await repository.GetAsync(message.Id)) == null) return;
-
-            await repository.RemoveAsync(message.Id);
-            
-            var likes = await likesRepository.GetAllAsync(like => like.FeedItemId == message.Id);
-            var comments = await commentsRepository.GetAllAsync(comment => comment.FeedItemId == message.Id);
-
-
-            foreach(var like in likes) {
-                await likesRepository.RemoveAsync(like.Id);
-                publishEndpoint.Publish(new LikeDeleted(like.Id));
-                }
-
-                
-            foreach(var comment in comments) {
-                await commentsRepository.RemoveAsync(comment.Id);
-                publishEndpoint.Publish(new CommentDeleted(comment.Id));
-                }
-        }
-        
-        #endregion
+      this.likesRepository = likesRepository;
+      this.commentsRepository = commentsRepository;
+      this.publishEndpoint = publishEndpoint;
     }
+    #endregion
+
+    #region Properties
+    private readonly IRepository<Like> likesRepository;
+    private readonly IRepository<Comment> commentsRepository;
+    private readonly IPublishEndpoint publishEndpoint;
+    #endregion
+
+    #region Methods
+
+    public async Task Consume(ConsumeContext<FeedItemDeleted> context)
+    {
+      FeedItemDeleted message = context.Message;
+      if ((await repository.GetAsync(message.Id)) == null)
+      {
+        return;
+      }
+
+      await repository.RemoveAsync(message.Id);
+
+      System.Collections.Generic.IReadOnlyCollection<Like> likes = await likesRepository.GetAllAsync(like => like.FeedItemId == message.Id);
+      System.Collections.Generic.IReadOnlyCollection<Comment> comments = await commentsRepository.GetAllAsync(comment => comment.FeedItemId == message.Id);
+
+
+      foreach (Like like in likes)
+      {
+        await likesRepository.RemoveAsync(like.Id);
+        await publishEndpoint.Publish(new LikeDeleted(like.Id));
+      }
+
+
+      foreach (Comment comment in comments)
+      {
+        await commentsRepository.RemoveAsync(comment.Id);
+        await publishEndpoint.Publish(new CommentDeleted(comment.Id));
+      }
+    }
+
+    #endregion
+  }
 }
--- src/Danstagram.Interactions.Service/Consumers/IdentityDeletedConsumer.cs
+++ src/Danstagram.Interactions.Service/Consumers/IdentityDeletedConsumer.cs
@@ -1,50 +1,57 @@
 using System.Threading.Tasks;
 using Danstagram.Common;
 using Danstagram.Identities.Contracts;
+using Danstagram.Interactions.Contracts;
 using Danstagram.Interactions.Service.Entities;
 using MassTransit;
 
-namespace Danstagram.Interactions.Service.Consumers{
-    public class IdentityDeletedConsumer : RepositoryConsumer<Identity>,IConsumer<IdentityDeleted>
+namespace Danstagram.Interactions.Service.Consumers
+{
+  public class IdentityDeletedConsumer : RepositoryConsumer<Identity>, IConsumer<IdentityDeleted>
+  {
+
+    #region Constructors
+    public IdentityDeletedConsumer(IRepository<Identity> repository, IRepository<Like> likesRepository, IRepository<Comment> commentsRepository, IPublishEndpoint publishEndpoint) : base(repository)
     {
-        
-        #region Constructors
-        public IdentityDeletedConsumer(IRepository<Identity> repository,IRepository<Like> likesRepository,IRepository<Comment> commentsRepository, IPublishEndpoint publishEndpoint) : base(repository)
-        {
-            this.likesRepository = likesRepository;
-            this.commentsRepository = commentsRepository;
-            this.publishEndpoint = publishEndpoint;
-        }
-        #endregion
-
-        #region Properties
-        private readonly IRepository<Like> likesRepository;
-        private readonly IRepository<Comment> commentsRepository;
-        private readonly IPublishEndpoint publishEndpoint;
-        #endregion
-        
-        #region Methods
-        public async Task Consume(ConsumeContext<IdentityDeleted> context)
-        {
-            var message = context.Message;
-            if ((await repository.GetAsync(message.Id)) == null) return;
-            
-            await this.repository.RemoveAsync(message.Id);
-
-            var likes = await likesRepository.GetAllAsync(like => like.UserId == message.Id);
-            var comments = await commentsRepository.GetAllAsync(comment => comment.UserId == message.Id);
-
-            foreach(var like in likes) {
-                await likesRepository.RemoveAsync(like.Id);
-                publishEndpoint.Publish(new LikeDeleted(like.Id));
-                }
-
-                
-            foreach(var comment in comments) {
-                await commentsRepository.RemoveAsync(comment.Id);
-                publishEndpoint.Publish(new CommentDeleted(comment.Id));
-                }
-        }
-        #endregion
+      this.likesRepository = likesRepository;
+      this.commentsRepository = commentsRepository;
+      this.publishEndpoint = publishEndpoint;
     }
+    #endregion
+
+    #region Properties
+    private readonly IRepository<Like> likesRepository;
+    private readonly IRepository<Comment> commentsRepository;
+    private readonly IPublishEndpoint publishEndpoint;
+    #endregion
+
+    #region Methods
+    public async Task Consume(ConsumeContext<IdentityDeleted> context)
+    {
+      IdentityDeleted message = context.Message;
+      if ((await repository.GetAsync(message.Id)) == null)
+      {
+        return;
+      }
+
+      await repository.RemoveAsync(message.Id);
+
+      System.Collections.Generic.IReadOnlyCollection<Like> likes = await likesRepository.GetAllAsync(like => like.UserId == message.Id);
+      System.Collections.Generic.IReadOnlyCollection<Comment> comments = await commentsRepository.GetAllAsync(comment => comment.UserId == message.Id);
+
+      foreach (Like like in likes)
+      {
+        await likesRepository.RemoveAsync(like.Id);
+        await publishEndpoint.Publish(new LikeDeleted(like.Id));
+      }
+
+
+      foreach (Comment comment in comments)
+      {
+        await commentsRepository.RemoveAsync(comment.Id);
+        await publishEndpoint.Publish(new CommentDeleted(comment.Id));
+      }
+    }
+    #endregion
+  }
 }
