23146adbe96d925228bf87fbfd48a988aa2fa5e3
DanShaul <dansh7836@gmail.com>
2023-11-28 19:50:49 +0200

    Finished

--- src/Danstagram.Interactions.Service/Controllers/InteractionsController.cs
+++ src/Danstagram.Interactions.Service/Controllers/InteractionsController.cs
@@ -6,15 +6,17 @@ using Danstagram.Common;
 using Danstagram.Interactions.Contracts;
 using Danstagram.Interactions.Service.Entities;
 using MassTransit;
-using MassTransit.Transports;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Danstagram.Interactions.Service.Controllers{
+namespace Danstagram.Interactions.Service.Controllers
+{
     [ApiController]
     [Route("interactions")]
-    public class InteractionsController : ControllerBase{
+    public class InteractionsController : ControllerBase
+    {
         #region Cosntructors
-        public InteractionsController(IRepository<FeedItem> feedItemsRepository,IRepository<Comment> commentsRepository,IRepository<Like> likesRepository,IRepository<Identity> identitiesRepository, IPublishEndpoint publishEndpoint){
+        public InteractionsController(IRepository<FeedItem> feedItemsRepository, IRepository<Comment> commentsRepository, IRepository<Like> likesRepository, IRepository<Identity> identitiesRepository, IPublishEndpoint publishEndpoint)
+        {
 
             this.feedItemsRepository = feedItemsRepository;
             this.commentsRepository = commentsRepository;
@@ -34,36 +36,45 @@ namespace Danstagram.Interactions.Service.Controllers{
 
         #endregion
         #region Methods
+        [Route("~/health")]
+        [HttpGet]
+        public ActionResult GetHealth()
+        {
+            return Ok();
+        }
 
         [Route("likes/feeditems/{id}")]
         [HttpGet]
-        public async Task<IEnumerable<LikeDto>> GetLikesByFeedItemsIdAsync(Guid feedItemId){
-            
+        public async Task<IEnumerable<LikeDto>> GetLikesByFeedItemsIdAsync(Guid feedItemId)
+        {
+
             return (await likesRepository.GetAllAsync(like => like.FeedItemId == feedItemId)).Select(like => like.AsDto());
         }
 
         [Route("likes/{id}")]
         [HttpGet]
-        public async Task<ActionResult<LikeDto>> GetLikeByIdAsync(Guid likeId){
-            var like = await likesRepository.GetAsync(likeId);
+        public async Task<ActionResult<LikeDto>> GetLikeByIdAsync(Guid likeId)
+        {
+            Like like = await likesRepository.GetAsync(likeId);
 
-            if (like == null) return NotFound();
-
-            return like.AsDto();
-
+            return like == null ? (ActionResult<LikeDto>)NotFound() : (ActionResult<LikeDto>)like.AsDto();
         }
 
 
         [Route("comments/{id}")]
         [HttpGet]
-        public async Task<ActionResult<CommentDto>> GetCommentByIdAsync(Guid commentId){
-            var comment = await commentsRepository.GetAsync(commentId);
+        public async Task<ActionResult<CommentDto>> GetCommentByIdAsync(Guid commentId)
+        {
+            Comment comment = await commentsRepository.GetAsync(commentId);
 
-            if (comment == null) return NotFound();
+            if (comment == null)
+            {
+                return NotFound();
+            }
 
-            var userName = (await identitiesRepository.GetAsync(comment.UserId)).UserName;
+            string userName = (await identitiesRepository.GetAsync(comment.UserId)).UserName;
 
-            var commentDto = new CommentDto(
+            CommentDto commentDto = new(
                 comment.Id,
                 userName,
                 comment.FeedItemId,
@@ -76,14 +87,15 @@ namespace Danstagram.Interactions.Service.Controllers{
 
         [Route("comments/feeditems/{id}")]
         [HttpGet]
-        public async Task<IEnumerable<CommentDto>> GetCommentsByFeedItemsIdAsync(Guid feedItemId){
-            var comments = await commentsRepository.GetAllAsync(comment => comment.FeedItemId == feedItemId);
+        public async Task<IEnumerable<CommentDto>> GetCommentsByFeedItemsIdAsync(Guid feedItemId)
+        {
+            IReadOnlyCollection<Comment> comments = await commentsRepository.GetAllAsync(comment => comment.FeedItemId == feedItemId);
 
-            var commentUserIds = comments.Select(x => x.UserId);
-            var identities = await identitiesRepository.GetAllAsync(identity => commentUserIds.Contains(identity.Id));
+            IEnumerable<Guid> commentUserIds = comments.Select(x => x.UserId);
+            IReadOnlyCollection<Identity> identities = await identitiesRepository.GetAllAsync(identity => commentUserIds.Contains(identity.Id));
 
 
-            var feedComments = comments.Select(comment => new CommentDto(
+            IEnumerable<CommentDto> feedComments = comments.Select(comment => new CommentDto(
                 comment.Id,
                 identities.SingleOrDefault(Identity => Identity.Id == comment.UserId).UserName,
                 comment.FeedItemId,
@@ -96,45 +108,59 @@ namespace Danstagram.Interactions.Service.Controllers{
 
         [Route("comments")]
         [HttpPost]
-        public async Task<ActionResult<CommentDto>> PostCommentAsync(CreateCommentDto createComment){
-            Comment comment = new(){
+        public async Task<ActionResult<CommentDto>> PostCommentAsync(CreateCommentDto createComment)
+        {
+            Comment comment = new()
+            {
                 Id = Guid.NewGuid(),
                 CreatedDate = DateTimeOffset.UtcNow,
                 FeedItemId = createComment.FeedItemId,
                 Message = createComment.Message,
                 UserId = createComment.UserId
             };
-            var validationResponse = await ValidateInteractionAsync<Comment>(comment);
-            if (validationResponse != null) return validationResponse;
+            ActionResult validationResponse = await ValidateInteractionAsync(comment);
+            if (validationResponse != null)
+            {
+                return validationResponse;
+            }
 
             await commentsRepository.CreateAsync(comment);
-            await publishEndpoint.Publish(new CommentCreated(comment.Id,comment.UserId,comment.FeedItemId,comment.Message,comment.CreatedDate));
+            await publishEndpoint.Publish(new CommentCreated(comment.Id, comment.UserId, comment.FeedItemId, comment.Message, comment.CreatedDate));
 
-            return CreatedAtAction(nameof(GetCommentByIdAsync),new{id = comment.Id},comment);
+            return CreatedAtAction(nameof(GetCommentByIdAsync), new { id = comment.Id }, comment);
         }
 
         [Route("likes")]
         [HttpPost]
-        public async Task<ActionResult<LikeDto>> PostLikeAsync(CreateLikeDto createLike){
-            Like like = new(){
+        public async Task<ActionResult<LikeDto>> PostLikeAsync(CreateLikeDto createLike)
+        {
+            Like like = new()
+            {
                 Id = Guid.NewGuid(),
                 FeedItemId = createLike.FeedItemId,
                 UserId = createLike.UserId
             };
 
-            var validationResponse = await ValidateInteractionAsync<Like>(like);
-            if (validationResponse != null) return validationResponse;
+            ActionResult validationResponse = await ValidateInteractionAsync(like);
+            if (validationResponse != null)
+            {
+                return validationResponse;
+            }
 
             await likesRepository.CreateAsync(like);
-            await publishEndpoint.Publish(new LikeCreated(like.Id,like.UserId,like.FeedItemId));
+            await publishEndpoint.Publish(new LikeCreated(like.Id, like.UserId, like.FeedItemId));
 
-            return CreatedAtAction(nameof(GetLikeByIdAsync),new{Id = like.Id},like);
+            return CreatedAtAction(nameof(GetLikeByIdAsync), new { like.Id }, like);
         }
 
         [Route("likes/{id}")]
         [HttpDelete]
-        public async Task<ActionResult> DeleteLikeAsync(Guid id) {
-            if(likesRepository.GetAsync(id) == null) return NoContent();
+        public async Task<ActionResult> DeleteLikeAsync(Guid id)
+        {
+            if (likesRepository.GetAsync(id) == null)
+            {
+                return NoContent();
+            }
 
             await likesRepository.RemoveAsync(id);
             await publishEndpoint.Publish(new LikeDeleted(id));
@@ -144,20 +170,24 @@ namespace Danstagram.Interactions.Service.Controllers{
 
         [Route("comments/{id}")]
         [HttpDelete]
-        public async Task<ActionResult> DeleteCommentsAsync(Guid id) {
-            if(commentsRepository.GetAsync(id) == null) return NoContent();
+        public async Task<ActionResult> DeleteCommentsAsync(Guid id)
+        {
+            if (commentsRepository.GetAsync(id) == null)
+            {
+                return NoContent();
+            }
 
             await commentsRepository.RemoveAsync(id);
             await publishEndpoint.Publish(new CommentDeleted(id));
-            
+
             return Ok();
         }
-        private async Task<ActionResult> ValidateInteractionAsync<T>(T t) where T : Interaction{
-            if((await feedItemsRepository.GetAsync(t.FeedItemId)) == null) return NotFound("Feed item not found");
-
-            if((await identitiesRepository.GetAsync(t.UserId)) == null) return NotFound("User not found");
-
-            return null;
+        private async Task<ActionResult> ValidateInteractionAsync<T>(T t) where T : Interaction
+        {
+            return (await feedItemsRepository.GetAsync(t.FeedItemId)) == null
+            ? NotFound("Feed item not found")
+            : (await identitiesRepository.GetAsync(t.UserId)) == null ? NotFound("User not found")
+            : (t is Like && (await likesRepository.GetAsync((entity) => entity.FeedItemId == t.FeedItemId && entity.UserId == t.UserId)) != null) ? Ok("Like is already created") : (ActionResult)null;
         }
         #endregion
     }
